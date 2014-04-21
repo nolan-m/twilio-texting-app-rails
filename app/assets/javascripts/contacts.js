019c0fa5426ef9164b18564eacd6a53ac5539615
Corey Norris & Nolan Miller <student@epicodus.com>
2014-04-21 16:50:39 -0700

    allow users to select contact from their contacts list to send a message

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -2,6 +2,14 @@ function initialize(){
   $('#save_contact').click(function(){
     $('#name_form').toggle();
   })
+  $('#contact-btn').click(function() {
+    $('#contacts-dropdown').toggle();
+    $('#to-field').toggle();
+    $('#save-check').toggle();
+    $(this).text(function(i, text){
+      return text === "New Contact" ? "Select From Contacts" : "New Contact";
+    });
+  });
 }
 
 $(document).ready(initialize);
